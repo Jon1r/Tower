98f42f7e82056495a7e3babb5df1ed6bc10e6702
Petr <petrsmirnov1995@gmail.com>
2020-09-25 22:09:42 +0300

    Update LightWarrior.gd

--- LightWarrior.gd
+++ LightWarrior.gd
@@ -1,11 +1,35 @@
 extends "res://Warrior.gd"
 
 var vel = Vector2(1, 0)
+var attack = false
+var see_enemy = false
 
 func _ready():
-	$Peasant/AnimationPlayer.play("walk")
+	pass
 
 func _physics_process(delta):
 	
-	move_and_slide(vel * speed * delta)
+	if see_enemy:
+		#$Sprite/Peasant/AnimationPlayer.stop("walk")
+		if attack:
+			attack = false
+			$Sprite/Peasant/AnimationPlayer.play("attack")
+	else:
+		#$Sprite/Peasant/AnimationPlayer.stop("attack")
+		$Sprite/Peasant/AnimationPlayer.play("walk")
+		move_and_slide(vel * speed * delta)
 	
+
+
+func _on_DetectRadius_body_entered(body):
+	see_enemy = true
+	attack = true
+	$Attack_speed.start()
+
+
+func _on_Attack_speed_timeout():
+	attack = true
+
+
+func _on_DetectRadius_body_exited(body):
+	see_enemy = false
