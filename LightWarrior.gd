795b2ad4e2f28dad7638105c90d90b7faf7ac3a2
Jon1r <68221445+Jon1r@users.noreply.github.com>
2020-09-25 19:19:15 +0300

    Добавил крестьянина и анимацию его

--- LightWarrior.gd
+++ LightWarrior.gd
@@ -2,8 +2,10 @@ extends "res://Warrior.gd"
 
 var vel = Vector2(1, 0)
 
-
+func _ready():
+	$Peasant/AnimationPlayer.play("walk")
 
 func _physics_process(delta):
 	
 	move_and_slide(vel * speed * delta)
+	
