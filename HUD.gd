727b78774613a1d568644275fc6a4ec21b9d71eb
Jon1r <68221445+Jon1r@users.noreply.github.com>
2020-09-27 22:51:02 +0300

    Update HUD.gd

--- HUD.gd
+++ HUD.gd
@@ -1,9 +1,17 @@
 extends CanvasLayer
 
-export (PackedScene) var light_warrior
+var light_warrior = preload("res://LightWarrior.tscn")
+var preasant = preload("res://Peasant.tscn")
 
 signal buy
 
 
 func _on_Button_pressed():
-	emit_signal("buy", light_warrior)
+	var warrior = light_warrior.instance()
+	warrior.set_enemy(preasant)
+	emit_signal("buy", warrior)
+	
+
+
+func _on_Button2_pressed():
+	pass # Replace with function body.
