f63777755c6df72809dc8ff7e5cf6eff7feb41a2
Jon1r <68221445+Jon1r@users.noreply.github.com>
2020-09-27 22:50:41 +0300

    Update LightWarrior.gd
    Теперь этот класс отвечающий за механику всех юнитов

--- LightWarrior.gd
+++ LightWarrior.gd
@@ -3,21 +3,38 @@ extends "res://Warrior.gd"
 var vel = Vector2(1, 0)
 var attack = false
 var see_enemy = false
+var enemy = null
+#Конструктор
+func _init():
+	print("hello")
+	
+func set_enemy(_enemy):
+	enemy = _enemy.instance()
+	$DetectRadius/CollisionShape2D.set_shape(enemy.get_detect_radius_shape())
+	$myShape.set_shape(enemy.get_collision())
+	add_child(enemy)
 
 func _ready():
 	pass
 
 func _physics_process(delta):
 	
+	if enemy == null:
+		return
 	if see_enemy:
 		#$Sprite/Peasant/AnimationPlayer.stop("walk")
 		if attack:
+			print(delta)
 			attack = false
-			$Sprite/Peasant/AnimationPlayer.play("attack")
+			enemy.play("attack")
 	else:
 		#$Sprite/Peasant/AnimationPlayer.stop("attack")
-		$Sprite/Peasant/AnimationPlayer.play("walk")
+		enemy.play("walk")
+		#this.actionEnemy.AnimationPlayer.play("walk")
 		move_and_slide(vel * speed * delta)
+		
+func explode():
+	queue_free()
 	
 
 
